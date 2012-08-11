744f73633fc5fc55f01212c391d17f46f7ba112e
Brad Parbs <brad@bradparbs.com>
2012-08-11 16:04:58 -0500

    First Commit, RAWR

---
+++ raptorize.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Plugin Name: wpRaptorize!
+Plugin URI: http://brrad.com/wpraptorize
+Description: Integrates Zurb Foundation's Raptorize jQuery plugin into WordPress. Type the konami code on any page, and have a raptor appear. Toggable sound, custom images, and more!
+Version: 10002
+Author: Brad Parbs
+Author URI: http://brrad.com
