bbb39c782afae80564e68310367d314586bb29de
Brad Parbs <brad@bradparbs.com>
2012-08-20 22:34:20 -0500

    This should all be rolled into the Konami Code plugin

--- raptorize.php
+++ raptorize.php
@@ -6,3 +6,30 @@ Description: Integrates Zurb Foundation's Raptorize jQuery plugin into WordPress
 Version: 10002
 Author: Brad Parbs
 Author URI: http://brrad.com
+*/
+function raptorizeOutput(){
+
+	wp_enqueue_script("jquery");
+
+	wp_register_script( 'jquery', '//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js');
+
+	wp_enqueue_script(
+	  "jquery.raptorize", RAWR_PATH . "/jquery.raptorize.1.0.js",
+	  array("jquery"), "",1);
+}
+
+add_action('wp_footer', 'raptorizeOutput',5);
+
+function rawwwrrrr(){
+	?>
+	<script type="text/javascript">
+		jQuery(window).load(function() {
+			jQuery('.button').raptorize({
+				'enterOn' : 'konami-code'
+				});w
+			});
+	</script>
+	<?
+}
+
+add_action('wp_head','rawwwrrrr',6);
